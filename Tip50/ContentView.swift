749e65092d2c9be4b77e53cae20a470223138f34
Eddie Christopher Fox <EddieChristopherFox@gmail.com>
2024-09-27 10:32:39 -1000

    Initial Commit

---
+++ Tip50/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tip50
+//
+//  Created by Eddie Christopher Fox III on 9/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tip50/Tip50App.swift
@@ -0,0 +1,17 @@
+//
+//  Tip50App.swift
+//  Tip50
+//
+//  Created by Eddie Christopher Fox III on 9/27/24.
+//
+
+import SwiftUI
+
+@main
+struct Tip50App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
