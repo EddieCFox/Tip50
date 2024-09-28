8f233a2e74a48751387f0ae40fad589946adcc50
Eddie Christopher Fox <EddieChristopherFox@gmail.com>
2024-09-27 21:32:00 -1000

    Finish MVP (Minimum Viable Product)

--- Tip50/ContentView.swift
+++ Tip50/ContentView.swift
@@ -8,17 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var billAmount = 0.0
+    @State var tipPercentage = 0.0
+    
+    // Computed properties for tip and total
+    // See: https://docs.swift.org/swift-book/documentation/the-swift-programming-language/properties/#
+    var tip: Double {
+        return billAmount * (tipPercentage / 100)
+    }
+
+    var total: Double {
+        return billAmount + tip
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+            // https://www.hackingwithswift.com/quick-start/swiftui/how-to-format-a-textfield-for-numbers
+                Text("Bill Amount:")
+                    .font(.headline)
+                    .padding(.leading, -10)
+                Text("$")
+                    .font(.headline)
+                    .fontWeight(.bold)
+                TextField("$0.00", value: $billAmount, format: .number)
+                    .textFieldStyle(.roundedBorder)
+                    .keyboardType(.decimalPad)
+            }
+            .padding()
+            
+            HStack {
+                Text("Tip:")
+                    .font(.headline)
+                Text("$\(tip, specifier: "%.2f")")
+            }
+            .padding()
+            
+            HStack {
+                Text("Grand Total:")
+                    .font(.headline)
+                Text("$\(total, specifier: "%.2f")")
+            }
+            
+            HStack {
+                Text("Tip percentage:")
+                    .font(.headline)
+                TextField("%", value: $tipPercentage, format: .number)
+                    .textFieldStyle(.roundedBorder)
+                    .keyboardType(.decimalPad)
+                Text("%")
+                    .font(.headline)
+                    .fontWeight(.bold)
+            }
+            .padding()
+            
+            Spacer()
         }
-        .padding()
     }
 }
-
 #Preview {
     ContentView()
 }
